6182a99bd6041b1f816c793273276880dd6a1b9a
KathiravanGnanam <87167379+KathiravanGnanam@users.noreply.github.com>
2024-05-20 12:55:44 +0530

    Update PriceControllerApplication.java

--- PriceController/src/main/java/com/blueyonder/PriceControllerApplication.java
+++ PriceController/src/main/java/com/blueyonder/PriceControllerApplication.java
@@ -3,12 +3,19 @@ package com.blueyonder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableScheduling
 public class PriceControllerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+	        // Set system properties
+	        System.setProperty("spring.datasource.url", dotenv.get("DATABASE_URL"));
+	        System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USERNAME"));
+	        System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD"));
 		SpringApplication.run(PriceControllerApplication.class, args);
 	}
 
